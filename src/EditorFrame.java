1cc655bd5e7a6850ca82bce8ea84d9714f759443
ArnauGarcia <arnau.garcia@gracia.lasalle.cat>
2025-03-10 18:16:13 +0100

    Feat: Add Submenus + TextArea

--- src/EditorFrame.java
+++ src/EditorFrame.java
@@ -1,40 +1,64 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class EditorFrame extends JFrame implements ActionListener{
 
-    EditorFrame() {
+    //text area initialization
+    protected JTextArea textArea = new JTextArea();
+    //menu initialization
+    protected JMenuBar menuBar = new JMenuBar();
 
+    //these are the three options
+    protected JMenu fileMenu = new JMenu("File");
+    protected JMenu editMenu = new JMenu("Edit");
+    protected JMenu helpMenu = new JMenu("Help");
+
+    //options inside "File"
+    protected JMenuItem openItem = new JMenuItem("Open");
+    protected JMenuItem saveItem = new JMenuItem("Save");
+    protected JMenuItem exitItem = new JMenuItem("Exit");
+
+    EditorFrame() {
         this.setTitle("Text Editor");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
         this.setSize(500,500);
-        this.setLayout(null);
+        this.setLocationRelativeTo(null);
 
-        JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("File");
-        JMenu editMenu = new JMenu("Edit");
-        JMenu helpMenu = new JMenu("Help");
+        //textArea declaration
+        this.add(textArea);
 
+        //menu items
+        this.setJMenuBar(menuBar);
         menuBar.add(fileMenu);
         menuBar.add(editMenu);
         menuBar.add(helpMenu);
-
-        JMenuItem openItem = new JMenuItem("Open");
-        JMenuItem saveItem = new JMenuItem("save");
-        JMenuItem exitItem = new JMenuItem("Exit");
-
         fileMenu.add(openItem);
         fileMenu.add(saveItem);
         fileMenu.add(exitItem);
-
-        this.setJMenuBar(menuBar);
-
+        openItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == exitItem) {
+            JLabel messageLabel = new JLabel("Are you sure you want to exit?", SwingConstants.CENTER);
 
+            int response = JOptionPane.showConfirmDialog(
+                    null,
+                    messageLabel,
+                    "Confirm Exit",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.PLAIN_MESSAGE
+            );
+
+            if (response == JOptionPane.YES_OPTION) {
+                System.exit(0);
+            }
+        }
     }
 }
--- src/TextEditor.java
+++ src/TextEditor.java
@@ -3,5 +3,6 @@ import javax.swing.*;
 public class TextEditor {
     public static void main(String[] args) {
         EditorFrame frame = new EditorFrame();
+
     }
 }
