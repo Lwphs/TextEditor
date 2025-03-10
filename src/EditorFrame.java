4a5a1f3b023d444b21eab8809615ec239576b287
ArnauGarcia <arnau.garcia@gracia.lasalle.cat>
2025-03-10 17:08:19 +0100

    Feat: Add Class TextEditor

---
+++ src/EditorFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class EditorFrame extends JFrame {
+
+    EditorFrame() {
+
+        this.setTitle("Text Editor");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setSize(500,500);
+        this.setLayout(null);
+
+    }
+}
---
+++ src/TextEditor.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class TextEditor {
+    public static void main(String[] args) {
+        EditorFrame frame = new EditorFrame();
+    }
+}
