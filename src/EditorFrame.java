7453af32bb13221b6793f478fa9a812670c0c1cc
ArnauGarcia <arnau.garcia@gracia.lasalle.cat>
2025-03-10 17:22:21 +0100

    Feat: Add Menu

--- src/EditorFrame.java
+++ src/EditorFrame.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class EditorFrame extends JFrame {
+public class EditorFrame extends JFrame implements ActionListener{
 
     EditorFrame() {
 
@@ -9,6 +11,30 @@ public class EditorFrame extends JFrame {
         this.setVisible(true);
         this.setSize(500,500);
         this.setLayout(null);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenu editMenu = new JMenu("Edit");
+        JMenu helpMenu = new JMenu("Help");
+
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(helpMenu);
+
+        JMenuItem openItem = new JMenuItem("Open");
+        JMenuItem saveItem = new JMenuItem("save");
+        JMenuItem exitItem = new JMenuItem("Exit");
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+
+        this.setJMenuBar(menuBar);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
