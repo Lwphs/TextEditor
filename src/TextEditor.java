7f94606726c3210afa5d601980d28bd23f3609f4
ArnauGarcia <arnau.garcia@gracia.lasalle.cat>
2025-04-03 19:07:19 +0200

    Feat: Match the System's theme

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -2,6 +2,12 @@ import javax.swing.*;
 
 public class TextEditor {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         EditorFrame frame = new EditorFrame();
 
     }
